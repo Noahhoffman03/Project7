4290e35e9056ae6f6339e06c2fd5daa3175edd07
Killerbees1912 <Killerbees1912@gmail.com>
2025-02-25 10:37:31 -0600

    Update and rename Goblin to Goblin.java

---
+++ src/edu/gac/mcs178/gack/domain/Goblin.java
@@ -0,0 +1,13 @@
+package edu.gac.mcs178.gack.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.gac.mcs178.gack.Utility;
+
+public class Goblin extends AutoPerson{
+public String name;
+  public Goblin(String name, Place place){
+
+  }
+}
